e238bb912fd1906f21b670639efca4fe5e096fa9
Yiyi ZHOU <49706913+yiyigit@users.noreply.github.com>
2020-07-27 10:02:16 -0700

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://e91ac5ad.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
